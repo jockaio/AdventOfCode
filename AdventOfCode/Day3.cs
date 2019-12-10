0e05c8a73a437ebe98052224f9392d1826f4f504
Joakim <joakimhellerstrom@hotmail.com>
2019-12-10 22:41:53 +0100

    day3

--- AdventOfCode/Day3.cs
+++ AdventOfCode/Day3.cs
@@ -1,14 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 
 namespace AdventOfCode
 {
     public class Day3 : IPuzzleSolver
     {
-        List<KeyValuePair<string, int>> WireA { get; set; }
-        List<KeyValuePair<string, int>> WireB { get; set; }
+        private List<KeyValuePair<string, int>> WireA { get; set; }
+        private List<KeyValuePair<string, int>> WireB { get; set; }
+
         public string GetName()
         {
             return "Day 3";
@@ -16,7 +16,8 @@ namespace AdventOfCode
 
         public void ParseInput()
         {
-            var input = File.ReadAllLines(@".\PuzzleInput\day3.txt").ToList();
+            var input = "R75, D30, R83, U83, L12, D49, R71, U7, L72\nU62,R66,U55,R34,D71,R55,D58,R83".Split("\n");
+            //var input = File.ReadAllLines(@".\PuzzleInput\day3.txt").ToList();
             WireA = ParseWirePath(input[0].Split(',').ToList());
             WireB = ParseWirePath(input[1].Split(',').ToList());
         }
@@ -25,15 +26,12 @@ namespace AdventOfCode
         {
             var coordinatesA = MapWire(WireA);
             var coordinatesB = MapWire(WireB);
-            var intersections = new List<KeyValuePair<int, int>>();
 
-            foreach (var item in coordinatesA)
-            {
-                if (coordinatesB.Exists(x => x.Key == item.Key && x.Value == item.Value))
-                {
-                    intersections.Add(item);
-                }
-            }
+            List<KeyValuePair<int, int>> intersections = FindIntersections(coordinatesA, coordinatesB);
+        }
+
+        private List<KeyValuePair<int, int>> FindIntersections(List<KeyValuePair<int, int>> coordinatesA, List<KeyValuePair<int, int>> coordinatesB)
+        {
         }
 
         private List<KeyValuePair<int, int>> MapWire(List<KeyValuePair<string, int>> wire)
@@ -43,6 +41,8 @@ namespace AdventOfCode
             var x = 0;
             var y = 0;
 
+            result.Add(new KeyValuePair<int, int>(x, y));
+
             foreach (var pathSection in wire)
             {
                 switch (pathSection.Key)
@@ -50,15 +50,19 @@ namespace AdventOfCode
                     case "R":
                         MoveOnXAxis(ref x, y, pathSection.Value, result);
                         break;
+
                     case "L":
                         MoveOnXAxis(ref x, y, pathSection.Value * -1, result);
                         break;
+
                     case "U":
                         MoveOnYAxis(ref y, x, pathSection.Value, result);
                         break;
+
                     case "D":
                         MoveOnYAxis(ref y, x, pathSection.Value * -1, result);
                         break;
+
                     default:
                         break;
                 }
@@ -69,24 +73,14 @@ namespace AdventOfCode
 
         private void MoveOnXAxis(ref int x, int y, int value, List<KeyValuePair<int, int>> result)
         {
-            int point;
-            for (int i = x; i < Math.Abs(value); i++)
-            {
-                point = value < 0 ? i * -1 : i; 
-                result.Add(new KeyValuePair<int, int>(point, y));
-            }
             x += value;
+            result.Add(new KeyValuePair<int, int>(x, y));
         }
 
         private void MoveOnYAxis(ref int y, int x, int value, List<KeyValuePair<int, int>> result)
         {
-            int point;
-            for (int i = y; i < Math.Abs(value); i++)
-            {
-                point = value < 0 ? i * -1 : i;
-                result.Add(new KeyValuePair<int, int>(x, point));
-            }
             y += value;
+            result.Add(new KeyValuePair<int, int>(x, y));
         }
 
         public void SolvePartTwo()
