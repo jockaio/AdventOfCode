9da3ac382061060cfa8fcc4c9f5338fe1dd2be45
Joakim <joakimhellerstrom@hotmail.com>
2019-12-12 22:49:14 +0100

    Solved day3 part one.

--- AdventOfCode/Day3.cs
+++ AdventOfCode/Day3.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace AdventOfCode
@@ -16,8 +17,10 @@ namespace AdventOfCode
 
         public void ParseInput()
         {
-            var input = "R75, D30, R83, U83, L12, D49, R71, U7, L72\nU62,R66,U55,R34,D71,R55,D58,R83".Split("\n");
-            //var input = File.ReadAllLines(@".\PuzzleInput\day3.txt").ToList();
+            //var input = "R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83".Split("\n");
+            //var input = "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7".Split("\n");
+            //var input = "R8,U5,L5,D3\nU7,R6,D4,L4".Split("\n");
+            var input = File.ReadAllLines(@".\PuzzleInput\day3.txt").ToList();
             WireA = ParseWirePath(input[0].Split(',').ToList());
             WireB = ParseWirePath(input[1].Split(',').ToList());
         }
@@ -28,59 +31,112 @@ namespace AdventOfCode
             var coordinatesB = MapWire(WireB);
 
             List<KeyValuePair<int, int>> intersections = FindIntersections(coordinatesA, coordinatesB);
+
+            List<int> results = intersections.Select(x => Math.Abs(x.Key) + Math.Abs(x.Value)).ToList();
+            results.Remove(0);
+
+            Console.WriteLine(results.Min());
         }
 
-        private List<KeyValuePair<int, int>> FindIntersections(List<KeyValuePair<int, int>> coordinatesA, List<KeyValuePair<int, int>> coordinatesB)
-        {
-        }
-
-        private List<KeyValuePair<int, int>> MapWire(List<KeyValuePair<string, int>> wire)
+        private List<KeyValuePair<int, int>> FindIntersections(List<Section> coordinatesA, List<Section> coordinatesB)
         {
             List<KeyValuePair<int, int>> result = new List<KeyValuePair<int, int>>();
 
-            var x = 0;
-            var y = 0;
-
-            result.Add(new KeyValuePair<int, int>(x, y));
-
-            foreach (var pathSection in wire)
+            foreach (var sectionA in coordinatesA)
             {
-                switch (pathSection.Key)
+                foreach (var sectionB in coordinatesB)
                 {
-                    case "R":
-                        MoveOnXAxis(ref x, y, pathSection.Value, result);
-                        break;
+                    if (sectionA.From.X == sectionA.To.X && sectionB.From.Y == sectionB.To.Y)
+                    {
+                        int min = sectionA.From.Y < sectionA.To.Y ? sectionA.From.Y : sectionA.To.Y;
+                        int max = sectionA.From.Y > min ? sectionA.From.Y : sectionA.To.Y;
 
-                    case "L":
-                        MoveOnXAxis(ref x, y, pathSection.Value * -1, result);
-                        break;
+                        if (sectionB.From.Y >= min && sectionB.From.Y <= max)
+                        {
+                            int minB = sectionB.From.X < sectionB.To.X ? sectionB.From.X : sectionB.To.X;
+                            int maxB = sectionB.From.X > minB ? sectionB.From.X : sectionB.To.X;
 
-                    case "U":
-                        MoveOnYAxis(ref y, x, pathSection.Value, result);
-                        break;
+                            if (sectionA.From.X >= minB && sectionA.From.X <= maxB)
+                            {
+                                result.Add(new KeyValuePair<int, int>(sectionA.From.X, sectionB.To.Y));
+                            }
+                        }
+                    }
 
-                    case "D":
-                        MoveOnYAxis(ref y, x, pathSection.Value * -1, result);
-                        break;
+                    if (sectionA.From.Y == sectionA.To.Y && sectionB.From.X == sectionB.To.X)
+                    {
+                        int min = sectionA.From.X < sectionA.To.X ? sectionA.From.X : sectionA.To.X;
+                        int max = sectionA.From.X > min ? sectionA.From.X : sectionA.To.X;
 
-                    default:
-                        break;
+                        if (sectionB.From.X >= min && sectionB.From.X <= max)
+                        {
+                            int minB = sectionB.From.Y < sectionB.To.Y ? sectionB.From.Y : sectionB.To.Y;
+                            int maxB = sectionB.From.Y > minB ? sectionB.From.Y : sectionB.To.Y;
+
+                            if (sectionA.From.Y >= minB && sectionA.From.Y <= maxB)
+                            {
+                                result.Add(new KeyValuePair<int, int>(sectionB.To.X, sectionA.From.Y));
+                            }
+                        }
+                    }
                 }
             }
 
             return result;
         }
 
-        private void MoveOnXAxis(ref int x, int y, int value, List<KeyValuePair<int, int>> result)
+        private List<Section> MapWire(List<KeyValuePair<string, int>> wire)
         {
-            x += value;
-            result.Add(new KeyValuePair<int, int>(x, y));
+            List<Section> result = new List<Section>();
+
+            var x = 0;
+            var y = 0;
+
+            Section section;
+
+            foreach (var pathSection in wire)
+            {
+                section = new Section();
+                section.From = new Coordinate(x, y);
+
+                switch (pathSection.Key)
+                {
+                    case "R":
+                        MoveOnXAxis(ref x, y, pathSection.Value, section);
+                        break;
+
+                    case "L":
+                        MoveOnXAxis(ref x, y, pathSection.Value * -1, section);
+                        break;
+
+                    case "U":
+                        MoveOnYAxis(ref y, x, pathSection.Value, section);
+                        break;
+
+                    case "D":
+                        MoveOnYAxis(ref y, x, pathSection.Value * -1, section);
+                        break;
+
+                    default:
+                        break;
+                }
+
+                result.Add(section);
+            }
+
+            return result;
         }
 
-        private void MoveOnYAxis(ref int y, int x, int value, List<KeyValuePair<int, int>> result)
+        private void MoveOnXAxis(ref int x, int y, int value, Section section)
+        {
+            x += value;
+            section.To = new Coordinate(x, y);
+        }
+
+        private void MoveOnYAxis(ref int y, int x, int value, Section section)
         {
             y += value;
-            result.Add(new KeyValuePair<int, int>(x, y));
+            section.To = new Coordinate(x, y);
         }
 
         public void SolvePartTwo()
@@ -98,5 +154,23 @@ namespace AdventOfCode
 
             return result;
         }
+    }
+
+    internal class Section
+    {
+        public Coordinate From { get; set; }
+        public Coordinate To { get; set; }
+    }
+
+    internal class Coordinate
+    {
+        public Coordinate(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int X { get; set; }
+        public int Y { get; set; }
     }
 }
