7069826928cf43671cdf7c644a4e94a0b2d7c39f
Joakim <joakimhellerstrom@hotmail.com>
2019-12-18 20:35:03 +0100

    Solved day4 and added stopwatch.

---
+++ AdventOfCode/Day4.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode
+{
+    public class Day4 : IPuzzleSolver
+    {
+        public List<int> Input { get; set; }
+
+        public string GetName()
+        {
+            return "Day 4";
+        }
+
+        public void ParseInput()
+        {
+            Input = new List<int> { 138307, 654504 };
+            //Input = new List<int> { 122244, 123444 };
+        }
+
+        public void SolvePartOne()
+        {
+            int combination = Input[0];
+            int end = Input[1];
+            int combinations = 0;
+
+            int[] numbersArray;
+
+            while (combination <= end)
+            {
+                numbersArray = GetNumbersArray(combination++);
+                if (!AllIncreasingNumbers(numbersArray))
+                {
+                    continue;
+                }
+
+                if (!ContaisDoubleNumber(numbersArray, false))
+                {
+                    continue;
+                }
+
+                combinations++;
+            }
+
+            Console.WriteLine("Number of combinations: " + combinations);
+        }
+
+        private int[] GetNumbersArray(int number)
+        {
+            var numbers = new Stack<int>();
+
+            for (; number > 0; number /= 10)
+                numbers.Push(number % 10);
+
+            return numbers.ToArray();
+        }
+
+        private bool AllIncreasingNumbers(int[] numbers)
+        {
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] < numbers[i - 1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private bool ContaisDoubleNumber(int[] numbers, bool partTwo)
+        {
+            int? validDouble = null;
+            int low = 2;
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (validDouble != null && numbers[i] != validDouble)
+                {
+                    break;
+                }
+                if (numbers[i] == numbers[i - 1])
+                {
+                    if (!partTwo)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        if (i >= low)
+                        {
+                            if (numbers[i - low] == numbers[i])
+                            {
+                                validDouble = null;
+                                continue;
+                            }
+                        }
+
+                        if (i + 1 < numbers.Length - 1)
+                        {
+                            if (numbers[i] == numbers[i + 1])
+                            {
+                                validDouble = null;
+                                continue;
+                            }
+                        }
+                        validDouble = numbers[i];
+                    }
+                }
+            }
+
+            return validDouble.HasValue;
+        }
+
+        public void SolvePartTwo()
+        {
+            //Test();
+            int combination = Input[0];
+            int end = Input[1];
+            int combinations = 0;
+
+            int[] numbersArray;
+
+            while (combination <= end)
+            {
+                numbersArray = GetNumbersArray(combination++);
+                if (!AllIncreasingNumbers(numbersArray))
+                {
+                    continue;
+                }
+
+                if (!ContaisDoubleNumber(numbersArray, true))
+                {
+                    continue;
+                }
+
+                combinations++;
+            }
+
+            Console.WriteLine("Number of combinations: " + combinations);
+        }
+
+        public void Test()
+        {
+            //int testNumber = 579999;
+            //int testNumber = 578999;
+            int testNumber = 578899;
+            var numbersArray = GetNumbersArray(testNumber);
+            Console.WriteLine(testNumber + " contains valid double numbers: " + ContaisDoubleNumber(numbersArray, true));
+        }
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace AdventOfCode
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             var day1 = new Day1();
             PrintResult(day1);
@@ -15,17 +16,46 @@ namespace AdventOfCode
             var day3 = new Day3();
             PrintResult(day3);
 
+            var day4 = new Day4();
+            PrintResult(day4);
+
             Console.ReadLine();
         }
 
-        static void PrintResult(IPuzzleSolver day)
+        private static void PrintResult(IPuzzleSolver day)
         {
-            Console.WriteLine(day.GetName());
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            Console.WriteLine("{0}{1}{2}", "-----", day.GetName(), "-----");
             day.ParseInput();
             Console.WriteLine("Result part 1: ");
             day.SolvePartOne();
+
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            PrintTimeElapsed(ts);
+
+            stopWatch.Reset();
+
+            stopWatch.Start();
+
             Console.WriteLine("Result part 2: ");
             day.SolvePartTwo();
+
+            stopWatch.Stop();
+            ts = stopWatch.Elapsed;
+            PrintTimeElapsed(ts);
+        }
+
+        private static void PrintTimeElapsed(TimeSpan ts)
+        {
+            // Format and display the TimeSpan value.
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            Console.WriteLine("RunTime " + elapsedTime);
+            Console.WriteLine("");
         }
     }
 }
